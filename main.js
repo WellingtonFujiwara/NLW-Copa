ab40946fe532da0b656eb820fc69372e2bc9c399
Wellington Fujiwara <w.fujiwara93@gmail.com>
2022-11-04 18:34:37 -0300

    add logo

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = 0;
+let delay = 0
 function createCard(date, day, games) {
-  delay = delay + 0.5;
+  delay = delay + 0.5
   return `
     <div class="card" style="animation-delay: ${delay}s">
       <h2>${date}<span>${day}</span></h2>
@@ -23,6 +23,27 @@ function createCard(date, day, games) {
 
 // `` para haver quebra de linha
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("korea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("cameroon", "16:00", "brazil")
+  )
